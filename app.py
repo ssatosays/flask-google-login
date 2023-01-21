601de3074c1d4afaf12b53690facf77c5ca431f9
ssatosays <ssatosays@gmail.com>
2023-01-21 18:01:08 +0900

    Add ssl_context

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    ssl_context = ("openssl/server.crt", "openssl/private.key")
+    app.run(debug=True, ssl_context=ssl_context)
