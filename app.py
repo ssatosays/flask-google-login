da7c38721eb78e6443113f01f4a241b8c50b00bd
ssatosays <ssatosays@gmail.com>
2023-01-21 18:04:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
+
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return "Unauthorized", 403
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    users = config.users
+    return users.get(user_id, None)
 
 
 @app.route("/")
